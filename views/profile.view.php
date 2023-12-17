85c7151eb723c28fb85eae9be75567840e023158
George Makura <makurag@proton.me>
2023-12-17 03:26:19 +0000

    Bit of refactoring

--- views/profile.view.php
+++ views/profile.view.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>Profile Page</title>
-	<link href="css/styles.css" rel="stylesheet" type="text/css">
+	<link href="../css/styles.css" rel="stylesheet" type="text/css">
+	<link href="../css/profile.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 	<nav>
@@ -15,7 +16,7 @@
 			<img src="profile-picture.jpg" alt="Profile Picture" width="150" height="150">
 			<p>Email: <?= htmlspecialchars($email, ENT_QUOTES, 'UTF-8') ?></p>
 			<p>Password: <?= htmlspecialchars($password, ENT_QUOTES, 'UTF-8') ?></p>
-			<button onclick="window.location.href = 'edit_profile.php';">Edit Profile</button>
+			<button onclick="window.location.href = '../php/edit_profile.php';">Edit Profile</button>
 		</div>
 	</main>
 	<footer>
