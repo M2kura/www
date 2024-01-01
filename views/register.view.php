9596106a09be02dea4a7fb263cb7fca200ac646e
George Makura <makurag@proton.me>
2024-01-01 21:30:41 +0000

    register page style

--- views/register.view.php
+++ views/register.view.php
@@ -4,26 +4,30 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/styles.css" />
+        <link rel="stylesheet" href="../css/register.css" />
         <title>la galerie | Sign Up</title>
     </head>
-    <body>
-        <div class="welcome">
-            <h1>Sign Up</h1>
+    <body class="reg-body">
+        <div class="register">
+            <h1><span class="create">Create</span> <span class="your">your</span> <span class="account">account.</span></h1>
         </div>
-        <div class="sign-in-form">
-            <form method="post" action="register.php" autocomplete="off">
-                <fieldset>
-                    <legend class="legend"><input type="Submit" value="Sign Up"></legend>
+        <div class="register-form">
+            <form method="post" action="register.php" autocomplete="off" enctype="multipart/form-data">
+                <fieldset class="main-fieldset">
+                    <legend class="legend"><input class="submit" type="Submit" value="Sign Up"></legend>
                     <label for="username">Username: <input type="text" id="username" name="username" required /></label>
                     <label for="password">Password: <input type="password" id="password" name="password" autocomplete="new-password" required /></label>
                     <label for="email">Email: <input type="email" id="email" name="email" required /></label>
+                    <label for="profile_picture">Profile Picture: 
+                        <input type="file" id="profile_picture" name="profile_picture" accept="image/*" />
+                    </label>
                 </fieldset>
             </form>
         </div>
         <div>
-            <p>Already have an account? <a href="index.php">Sign In</a></p>
+            <p>Already have an account? <a href="../index.html">Sign In</a></p>
             <?php if (!empty($message)) : ?>
-                <p><?= $message ?></p>
+                <p class="success"><?= $message ?></p>
             <?php endif; ?>
         </div>
     </body>
