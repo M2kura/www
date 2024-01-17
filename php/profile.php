93fdbe3486a343ad0ad74d58998831a0e9d32ab2
George Makura <makurag@proton.me>
2024-01-17 11:57:44 +0000

    Better routing for profile page

--- php/profile.php
+++ php/profile.php
@@ -5,22 +5,22 @@ session_start();
 require "non_login_redirect.php";
 require "db_connection.php";
 
-// Get the id from the URL, or use the logged-in user's id if no id is specified in the URL
-$id = isset($_GET['id']) ? $_GET['id'] : $_SESSION['id'];
+// Get the username from the URL parameter
+$username = $_GET['username'];
+$errors = array();
 
-$sql = "SELECT * FROM accounts WHERE id = ?";
+$sql = "SELECT * FROM accounts WHERE username = ?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $id);
+$stmt->bind_param("s", $username);
 $stmt->execute();
 $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
     $userData = $result->fetch_assoc();
 } else {
-    $message = "User not found";
+    array_push($errors, "User does not exist");
 }
 
-$isOwner = $id == $_SESSION['id'];
-//script for updating profile picture]
+$isOwner = $username == $_SESSION['username'];
 
 require "../views/profile.view.php";
--- views/profile.view.php
+++ views/profile.view.php
@@ -11,22 +11,30 @@
 		<?php require('partials/nav.php'); ?>
 		<div class="container">
 			<h1>Profile Page</h1>
-			<div class="profile">
-				<div class="profile-picture">
-					<img height="150px" width="150px" src="<?php echo $userData['profile_pic']; ?>" alt="Profile Picture">
-				</div>
-				<div class="profile-info">
-					<div class="profile-name">
-						<h2>Username: <?php echo $userData['username']; ?></h2>
+			<?php if (empty($errors)): ?>
+				<div class="profile">
+					<div class="profile-picture">
+						<img height="150px" width="150px" src="<?php echo $userData['profile_pic']; ?>" alt="Profile Picture">
 					</div>
-					<?php if ($isOwner): ?>
-						<div class="edit-profile">
-							<a href="edit_profile.php">Edit Profile</a>
+					<div class="profile-info">
+						<div class="profile-name">
+							<h2>Username: <?php echo $userData['username']; ?></h2>
 						</div>
-					<?php endif; ?>
+						<?php if ($isOwner): ?>
+							<div class="edit-profile">
+								<a href="edit_profile.php">Edit Profile</a>
+							</div>
+						<?php endif; ?>
+					</div>
 				</div>
-			</div>
+			<?php endif; ?>
+			<?php if (!empty($errors)): ?>
+				<div class="errors">
+					<?php foreach ($errors as $error): ?>
+						<p><?php echo $error; ?></p>
+					<?php endforeach; ?>
+				</div>
+			<?php endif; ?>
 		</div>
-		<script src="../js/profile_pic_change.js"></script>
 	</body>
 </html>
